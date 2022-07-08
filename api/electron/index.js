6e1c0fae5a3dad3fead76b16fa79410e76aba460
Pluto Rotegott <rotegott@dogma-project.org>
2022-07-08 20:39:41 +0300

    electron test

--- api/electron/index.js
+++ api/electron/index.js
@@ -1,51 +1,14 @@
-const { ipcMain } = require("electron");
-
-const Server = require("../main/index");
+const Server = require("../socket.io/index");
 const filesHandler = require("./files");
 
 class ElectronServer extends Server {
 
-	constructor() {
-		super();
-		this.bridge = ipcMain;
-	}
-
-	/**
-	 * 
-	 * @param {String} type 
-	 * @param {String} responseType 
-	 * @param {Functio} getter async
-	 */
-	_on(type, responseType, getter) {
-		this.bridge.on(type, async (event, data) => {
-			const result = await getter(data);
-			event.reply(responseType, result);
-		});
-	}
-
-	/**
-	 * 
-	 * @param {String} type 
-	 * @param {Function} getter async
-	 */
-	_handle(type, getter) {
-		this.bridge.handle(type, async (_event, data) => {
-			const result = await getter(data);
-			return result;
-		});
-	}
-
-	/**
-	 * 
-	 * @param {String} type 
-	 * @param {*} data 
-	 */
-	_broadcast(type, data) {
-		global.win && global.win.webContents.send(type, data);
-	}
+	// constructor() {
+	// 	super();
+	// }
 
 	_filesHandler() {
-		super._filesHandler();
+		// super._filesHandler(); // check
 		filesHandler.call(this);
 	}
 

89255601a1bfca91e0440d4d5a21a16b078b0d41
Pluto Rotegott <rotegott@dogma-project.org>
2022-07-09 21:55:53 +0300

    datadir info

--- modules/datadir.js
+++ modules/datadir.js
@@ -1,9 +1,10 @@
 const args = require("./arguments");
+const logger = require("../logger");
 
 const os = require("os");
 
 let homedir;
-if (os.platform() === "android") {
+if (os.platform() == "android") {
     homedir = os.homedir().split("/").slice(0, 4).join("/");
 } else {
     homedir = os.homedir();
@@ -11,5 +12,7 @@ if (os.platform() === "android") {
 
 const dogmaDir = homedir + "/.dogma-node";
 const datadir = dogmaDir + (args.prefix ? `/${args.prefix}` : "/default");
+
+logger.info("datadir", datadir, "platform:", os.platform());
 
 module.exports = { datadir, dogmaDir };

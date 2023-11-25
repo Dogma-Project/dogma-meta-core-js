b5d6a0a1944157056c4ce6c4fe52f703b2858478
Pluto Rotegott <rotegott@dogma-project.org>
2023-11-25 20:19:46 +0200

    fix tsc compilled

--- types/index.d.ts
+++ types/index.d.ts
@@ -14,5 +14,12 @@ declare const State: {
 declare const System: {
     run: typeof run;
     getArg: typeof getArg;
+    logger: {
+        error: (type: string, ...message: any) => void;
+        warn: (type: string, ...message: any) => void;
+        info: (type: string, ...message: any) => void;
+        log: (type: string, ...message: any) => void;
+        debug: (type: string, ...message: any) => void;
+    };
 };
 export { System, Keys, State, Model, Types, connections as Connections };

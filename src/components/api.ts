4003fba169b17acb22e19be63b779c96267d43e7
Pluto Rotegott <rotegott@dogma-project.org>
2023-12-13 14:52:00 +0200

    broadcast logs

--- src/components/api.ts
+++ src/components/api.ts
@@ -3,11 +3,12 @@ import { workerData } from "node:worker_threads";
 import WebSocketApi from "../modules/api";
 import stateManager from "./state";
 import { C_API, C_Event } from "@dogma-project/constants-meta";
-// import logger from "../modules/logger";
+import logger from "../modules/logger";
 
 const wsApi = new WebSocketApi(workerData.apiPort);
 
 stateManager.subscribe([C_Event.Type.services], ([services]) => {
+  logger.log("services", "Broadcast", services.length);
   wsApi.broadcast({
     type: C_API.ApiRequestType.services,
     action: C_API.ApiRequestAction.set,

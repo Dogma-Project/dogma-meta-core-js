bc19d7628e1762aea0b87dbd0e6c15817fd59ff0
Pluto Rotegott <rotegott@dogma-project.org>
2024-04-11 21:44:58 +0300

    async check home dir + build docs task

--- src/modules/checkHomeDir.ts
+++ src/modules/checkHomeDir.ts
@@ -1,21 +1,25 @@
-import fs from "node:fs";
+import fs from "node:fs/promises";
 import { workerData } from "node:worker_threads";
 import dir from "./datadir";
 
-/**
- * @todo check!!!
- * @returns
- */
-export default function checkHomeDir() {
+export default async function checkHomeDir() {
   try {
     if (
       workerData.prefix.indexOf("empty-") > -1 ||
       workerData.prefix.indexOf("test-") > -1
     ) {
-      fs.rmSync(dir.data, { recursive: true, force: true });
+      await fs.rm(dir.data, { recursive: true, force: true });
     }
-    if (!fs.existsSync(dir.nedb)) fs.mkdirSync(dir.nedb, { recursive: true });
-    if (!fs.existsSync(dir.keys)) fs.mkdirSync(dir.keys, { recursive: true });
+    try {
+      await fs.access(dir.nedb);
+    } catch (_err) {
+      await fs.mkdir(dir.nedb, { recursive: true });
+    }
+    try {
+      await fs.access(dir.keys);
+    } catch (_err) {
+      await fs.mkdir(dir.keys, { recursive: true });
+    }
     return Promise.resolve(true);
   } catch (err) {
     return Promise.reject(err);
